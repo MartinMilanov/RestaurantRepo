67c86968a6ed06fa05a4b6fc8f0068ce495dd9ec
MartinMilanov <mmilanov999@gmail.com>
2022-06-04 11:49:26 +0300

    base pagination visuals

--- frontend/src/components/shared/list.jsx
+++ frontend/src/components/shared/list.jsx
@@ -1,81 +1,94 @@
+import Pagination from "./pagination";
+
 const List = () => {
+  const object = { name: "hello", age: "22", hasABowl: "nobro" };
+
+  const fillTableHeaders = () => {
+    return (
+      <>
+        {Object.keys(object).map((x, index) => (
+          <th scope="col" className={"onHoverTh"} key={index}>
+            {x.charAt(0).toUpperCase() + x.slice(1)}
+          </th>
+        ))}
+      </>
+    );
+  };
+
   return (
-    <table class="table">
-      <thead class="thead-light">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col" className={"onHoverTh"}>
-            First
-          </th>
-          <th scope="col" className={"onHoverTh"}>
-            Last
-          </th>
-          <th scope="col" className={"onHoverTh"}>
-            Handle
-          </th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>
-            <button class="btn btn-primary listBtn">
-              <i class="bi bi-eye"></i>
-            </button>
+    <>
+      <table className="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            {fillTableHeaders()}
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
+              <button class="btn btn-primary listBtn">
+                <i class="bi bi-eye"></i>
+              </button>
 
-            <button class="btn btn-warning listBtn">
-              <i class="bi bi-pencil-square"></i>
-            </button>
+              <button class="btn btn-warning listBtn">
+                <i class="bi bi-pencil-square"></i>
+              </button>
 
-            <button class="btn btn-danger listBtn">
-              <i class="bi bi-trash"></i>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>
-            <button class="btn btn-primary listBtn">
-              <i class="bi bi-eye"></i>
-            </button>
+              <button class="btn btn-danger listBtn">
+                <i class="bi bi-trash"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
+              <button class="btn btn-primary listBtn">
+                <i class="bi bi-eye"></i>
+              </button>
 
-            <button class="btn btn-warning listBtn">
-              <i class="bi bi-pencil-square"></i>
-            </button>
+              <button class="btn btn-warning listBtn">
+                <i class="bi bi-pencil-square"></i>
+              </button>
 
-            <button class="btn btn-danger listBtn">
-              <i class="bi bi-trash"></i>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>
-            <button class="btn btn-primary listBtn">
-              <i class="bi bi-eye"></i>
-            </button>
+              <button class="btn btn-danger listBtn">
+                <i class="bi bi-trash"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
+              <button class="btn btn-primary listBtn">
+                <i class="bi bi-eye"></i>
+              </button>
 
-            <button class="btn btn-warning listBtn">
-              <i class="bi bi-pencil-square"></i>
-            </button>
+              <button class="btn btn-warning listBtn">
+                <i class="bi bi-pencil-square"></i>
+              </button>
 
-            <button class="btn btn-danger listBtn">
-              <i class="bi bi-trash"></i>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              <button class="btn btn-danger listBtn">
+                <i class="bi bi-trash"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div style={{ display: "flex" }}>
+        <Pagination />
+      </div>
+    </>
   );
 };
 
