8e9364f9d92a7cbeec84b33004194e3b059bec4c
MartinMilanov <mmilanov999@gmail.com>
2022-06-04 08:59:32 +0300

    Added frontend skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary"> hello </button>
+    </div>
+  );
+}
+
+export default App;
