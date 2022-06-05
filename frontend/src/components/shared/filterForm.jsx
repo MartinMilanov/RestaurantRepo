239166ca091cad4f47f255e3b9abced04f3720fc
MartinMilanov <mmilanov999@gmail.com>
2022-06-05 11:40:40 +0300

    added current filter remove buttons

--- frontend/src/components/shared/filterForm.jsx
+++ frontend/src/components/shared/filterForm.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react";
 const FilterForm = ({ filters }) => {
+  const [activeFilterKvpArr, setActiveFilterKvpArr] = useState([
+    ["Name", "Todd"],
+    ["Vanish", "Todd"],
+  ]);
+
   const generateFilters = () => {
     var dataKvpFilters = [];
 
@@ -19,12 +25,41 @@ const FilterForm = ({ filters }) => {
     ));
   };
 
+  const generateActiveFilterButtons = () => {
+    return activeFilterKvpArr.map((x, index) => (
+      <button
+        key={`${index}-filterRemove`}
+        type="button"
+        class="btn btn-primary btn-filter-remove"
+        onClick={() => removeFilter(x[0])}
+      >
+        {x[0]} : {x[1]} <span class="badge badge-light">x</span>
+      </button>
+    ));
+  };
+
+  const removeFilter = (filterName) => {
+    if (activeFilterKvpArr.length > 0) {
+      var newState = activeFilterKvpArr;
+      var indexOfFilter = activeFilterKvpArr.findIndex(
+        (x) => x[0] == filterName
+      );
+      newState.splice(indexOfFilter, 1);
+      setActiveFilterKvpArr([...newState]);
+    }
+  };
+
   return (
     <div className="filterBlock">
       <h3>Filters</h3>
+
       {generateFilters()}
 
       <button className="btn btn-primary">Filter</button>
+
+      <div className="btn-filter-container">
+        {generateActiveFilterButtons()}
+      </div>
     </div>
   );
 };
